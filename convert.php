efe08ca6c29fea2a2bffa7bade20d7b2469279e4
Manuel Silvoso <msilvoso@gmail.com>
2020-04-15 17:29:59 +0200

    readability

--- convert.php
+++ convert.php
@@ -12,9 +12,21 @@ $run->hideColumns(11);
 // add field options (see b-table of Bootstrapvue) info is blue, danger is red
 $run->setFieldOption(0,'variant','info');
 // colorize rows
-$run->setRowOptions(11, 'danger');
-$run->setRowOptions(10, 'warning', ConvertToSearchableHtmlPage::CELL_IS_GREATER_OR_EQUAL, 9, ConvertToSearchableHtmlPage::ONLY_CELL);
-$run->setRowOptions(10, 'success', ConvertToSearchableHtmlPage::CELL_IS_SET + ConvertToSearchableHtmlPage::CELL_IS_LOWER, 9, ConvertToSearchableHtmlPage::ONLY_CELL);
+$run->setRowOptions(11, 'danger',
+    ConvertToSearchableHtmlPage::CELL_IS_SET,
+    0,
+    ConvertToSearchableHtmlPage::WHOLE_ROW
+);
+$run->setRowOptions(10, 'warning',
+    ConvertToSearchableHtmlPage::CELL_IS_GREATER_OR_EQUAL,
+    9,
+    ConvertToSearchableHtmlPage::ONLY_CELL
+);
+$run->setRowOptions(10, 'success',
+    ConvertToSearchableHtmlPage::CELL_IS_SET + ConvertToSearchableHtmlPage::CELL_IS_LOWER,
+    9,
+    ConvertToSearchableHtmlPage::ONLY_CELL
+);
 // define the columns thta will be searchable
 $run->setSearchableFields([0,2,7]);
 // set title of the page
